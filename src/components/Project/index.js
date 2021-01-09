f0fb95c0a4c2caf3addc3213253c9c67f05267b7
Fernando Angulo Donoso <fangulo82@gmail.com>
2021-01-09 11:56:28 -0800

    fixed buttons

--- src/components/Project/index.js
+++ src/components/Project/index.js
@@ -4,19 +4,25 @@ import Button from 'react-bootstrap/Button';
 import "./style.css";
 
 export default function Project(props) {
-    return (
-        <div className="parent">
-        <Card className="card">
-          <Card.Img variant="top" src={props.image} />
-          <Card.Body className="card-body-content">
-            <Card.Title>{props.title}</Card.Title>
-            <Card.Text>{props.description}</Card.Text>
-            <div className="card-button">
-              <a href={props.url} target="_blank"><Button variant="primary">Visit Project</Button></a>
-            </div>
-          </Card.Body>
-        </Card>
-      </div>
-    );
+
+  const handleClick = (e) => {
+    e.preventDefault()
+    window.open(props.url, "_blank");
   }
+  
+  return (
+    <div className="parent">
+      <Card className="card">
+        <Card.Img variant="top" src={props.image} />
+        <Card.Body className="card-body-content">
+          <Card.Title>{props.title}</Card.Title>
+          <Card.Text>{props.description}</Card.Text>
+          <div className="card-button">
+            <Button variant="primary" onClick={handleClick}>Visit Project</Button>
+          </div>
+        </Card.Body>
+      </Card>
+    </div>
+  );
+}
 
