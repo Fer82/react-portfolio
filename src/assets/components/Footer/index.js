6197967b7e23317d182204ec66dbc850f83644ba
Fernando Angulo Donoso <fangulo82@gmail.com>
2021-02-24 20:56:27 -0800

    Footer index.js and style.css

---
+++ src/assets/components/Footer/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./style.css";
+
+function Footer() {
+    return (
+        <footer className="footer copyright page-footer">
+            <span className="text-black">©Copyright</span>
+        </footer>
+    );
+}
+
+export default Footer;
